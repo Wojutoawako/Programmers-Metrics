224a9e909eeb155660a3954176ff1519d12fa16e
Egor.Chizhov <elshmelle@ya.ru>
2026-05-26 17:05:02 +0500

    Добавлен authRedirectGuard
    authRedirectGuard перенаправляет на главную в случае, если существует токен авторизации

--- src/app/guards/auth-redirect-guard.ts
+++ src/app/guards/auth-redirect-guard.ts
@@ -1,10 +1,10 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authRedirectGuard: CanActivateFn = (route, state) => {
     const router: Router = inject(Router);
 
     const token: string | null = localStorage.getItem('gitlab_token');
 
-    return !!token || router.parseUrl('auth');
+    return !token || router.parseUrl('list');
 };
