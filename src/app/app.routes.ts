14eb885c5bf909b9d8c8704145ed818be7b125c0
Egor.Chizhov <elshmelle@ya.ru>
2026-05-26 17:03:32 +0500

    Добавлены guards для аутентификации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,29 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page.component/login-page.component';
 import { CallbackComponent } from './pages/callback.component/callback.component';
+import { ProgrammersList } from './pages/programmers-list/programmers-list';
+import { authGuard } from './guards/auth-guard';
+import { authRedirectGuard } from './guards/auth-redirect-guard';
 
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
         component: LoginPageComponent,
     },
     {
         path: 'auth/callback',
         component: CallbackComponent,
-    }
+        canActivate: [authRedirectGuard],
+    },
+    {
+        path: 'list',
+        component: ProgrammersList,
+        canActivate: [authGuard],
+    },
 ];
 
