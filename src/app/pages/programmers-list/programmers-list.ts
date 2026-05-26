2070e3a7f203b2d0bbb456cac11d2c7b19d00b68
Egor.Chizhov <elshmelle@ya.ru>
2026-05-26 17:10:11 +0500

    Работа с сигналами и формами Angular
    Выводимая информация представлена сигналами. Также добавлена FormGroup для формы поиска.

--- src/app/pages/programmers-list/programmers-list.ts
+++ src/app/pages/programmers-list/programmers-list.ts
@@ -1,81 +1,77 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, Signal, signal, WritableSignal } from '@angular/core';
 import { IProgrammerData } from '../../interfaces/programmer-data.interface';
-import { ListRequestService } from '../../services/list-request.service';
 import { MatList, MatListItem, MatListItemLine, MatListItemTitle } from '@angular/material/list';
-import { MatCard, MatCardActions, MatCardContent, 
+import { MatCard, MatCardActions, MatCardContent,
     MatCardAvatar, MatCardTitleGroup, MatCardTitle, MatCardSubtitle } from '@angular/material/card';
-import { Router } from '@angular/router';
+import { ListRequestService } from '../../services/list-request/list-request.service';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, startWith } from 'rxjs';
 
 @Component({
     selector: 'app-programmers-list',
     imports: [MatList, MatListItem, MatListItemLine, MatListItemTitle,
-    MatCard, MatCardActions, MatCardContent,
+    MatCard, MatCardActions, MatCardContent, FormsModule, ReactiveFormsModule,
     MatCardAvatar, MatCardTitleGroup, MatCardTitle, MatCardSubtitle],
+    providers: [ListRequestService],
+    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './programmers-list.html',
     styleUrl: './programmers-list.scss',
 })
 export class ProgrammersList {
-    private listRequest: ListRequestService = inject(ListRequestService);
-    // private router: Router = inject(Router);
-    
-    private _programmersList: IProgrammerData[] = [];
+    private listRequestService: ListRequestService = inject(ListRequestService);
+    private destroyRef: DestroyRef = inject(DestroyRef);
 
-    private _displayList: IProgrammerData[] = [];
+    public programmersList: Signal<IProgrammerData[]> =
+        toSignal(
+            this.listRequestService.getUsers()
+                .pipe(
+                    takeUntilDestroyed(this.destroyRef),
+                ),
+            { initialValue: [] },
+        );
 
-    public get displayList() {
-        return this._displayList;
-    }
+    public filteredProgrammersList: WritableSignal<IProgrammerData[]> = signal<IProgrammerData[]>([]);
 
-    private set displayList(value: IProgrammerData[]) {
-        this._displayList = value;
-    }
-    
+    public filterFormGroup = new FormGroup({ nameFilter: new FormControl<string>('') });
+
+    public isCompareButtonActive: WritableSignal<boolean> = signal(false);
+
     constructor() {
-        this.listRequest.getUsers().subscribe(users => this._programmersList = users);
-        // this.listRequest.getUsersTest().subscribe(users => this._programmersList = users);
-        this.displayList = this._programmersList;
-    }
-
-    /**Реакция на получение ввода в фильтре по имени */
-    public onNameFilterInputChanged(event: Event) {
-        const value: string = (event.target as HTMLInputElement).value;
-
-        if (!value) {
-            this.displayList = this._programmersList;
-        } else {
-            this.displayList = this._programmersList.filter(user => {
-                return user.name.toLowerCase().includes(value.toLowerCase());
+        this.filterFormGroup.valueChanges
+            .pipe(
+                startWith({
+                    nameFilter: '',
+                }),
+                debounceTime(500),
+                takeUntilDestroyed(this.destroyRef),
+            )
+            .subscribe(filter => {
+                this.filteredProgrammersList.set(this.programmersList()
+                    .filter(user =>
+                        user.name.toLowerCase().includes(filter.nameFilter!.toLowerCase()),
+                    ));
             });
-        }
     }
 
     /**Реакция на нажатие кнопки "Добавить в сравнение" */
     public onAddUserButtonClicked(ev: PointerEvent, username: string) {
-        const targetButton = (ev.target as HTMLButtonElement);
-
-        // toggle
-        if (targetButton.textContent === 'Добавить в сравнение') {
-            targetButton.classList.add('button-active');
-            targetButton.textContent = 'Убрать из сравнения';
-
-            this.listRequest.addUserToComparison(username);
+        if (!this.isCompareButtonActive()) {
+            this.listRequestService.addUserToComparison(username);
         } else {
-            targetButton.classList.remove('button-active');
-            targetButton.textContent = 'Добавить в сравнение';
-
-            this.listRequest.removeUserFromComparison(username);
+            this.listRequestService.removeUserFromComparison(username);
         }
+
+        this.isCompareButtonActive.set(!this.isCompareButtonActive());
     }
 
-    /**Переход на страницу подробным содержанием о пользователе */
+    /**TODO */
     public onDetailsButtonClicked(username: string) {
-        // this.router.navigateByUrl(`details/${username}`);
-        console.log(`navigate to details/${username}`);
+
     }
 
-    /**Переход к странице сравнения */
+    /**TODO */
     public onCompareButtonClicked() {
-        // this.router.navigateByUrl('compare');
-        console.log('navigate to compare');
+
     }
 }
